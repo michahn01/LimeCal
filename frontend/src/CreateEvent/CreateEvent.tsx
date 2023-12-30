ea1169ad51e69636f18188d220a4574486c03f93
Jisang Ahn <michahn@umich.edu>
2023-12-30 14:16:22 -0800

    Slider added to event creation page

--- frontend/src/CreateEvent/CreateEvent.tsx
+++ frontend/src/CreateEvent/CreateEvent.tsx
@@ -1,6 +1,20 @@
 import React, { useState } from "react"
+import ReactSlider from 'react-slider'; 
+
 import "./CreateEvent.css"
 import DateSelector from "./DateSelector";
+
+// converts hh:mm:ss to "X AM" or "Y PM" format
+const convertTimestamp = (timestamp: string): string => {
+    const hoursString = timestamp.split(':')[0];
+    let hours = parseInt(hoursString);
+
+    const amPm = (hours < 12 || hours === 24) ? 'AM' : 'PM';
+    hours = hours % 12;
+    hours = hours === 0 ? 12 : hours;
+
+    return `${hours} ${amPm}`;
+}
 
 const Create = () => {
     const [eventTitle, setEventTitle] = useState<string>("");
@@ -9,6 +23,15 @@ const Create = () => {
     const [submitMessage, setSubmitMessage] = useState<string>("");
 
     const [calSelectedDates, setCalSelectedDates] = useState<string[]>([]);
+
+    // viewWindowRange contains the inclusive start and inclusive end times of the 
+    // viewing window of the selection panel. The user can adjust the range of time 
+    // slots using the 'time-range-selector-slider'.
+    const [viewWindowRange, setViewWindowRange] = useState<string[]>(["08:00", "17:00"]);
+
+    // labels on the time-range-selector-slider
+    const [sliderLabels, setSliderLabels] = useState(["8 AM", "5 PM"]);
+
 
     const sendApiRequest = () => {
 
@@ -38,6 +61,19 @@ const Create = () => {
             setPassword2("");
         }
     }
+
+    // callback function for when user uses the time range slider to adjust the
+    // time interval to be displayed on the selection panel's viewing window.
+    // thumbValue := values of the thumbs on the slider.
+    const sliderAfterChange = (thumbValue: number[], _: number) => {
+        const newRange: string[] = [`${thumbValue[0]}:00`.padStart(5, '0'), `${thumbValue[1]}:00`.padStart(5, '0')]
+        setViewWindowRange(newRange);
+    }
+    const sliderOnChange = (thumbValue: number[], _: number) => {
+        const newRange: string[] = [`${thumbValue[0]}:00`.padStart(5, '0'), `${thumbValue[1]}:00`.padStart(5, '0')]
+        setSliderLabels(newRange);
+    }
+
     return (
         <div id="page-body">
             <div className="form-container">
@@ -55,6 +91,30 @@ const Create = () => {
                 <center>
                 <DateSelector></DateSelector>
                 </center>
+
+                <div className='slider-area'>
+                <div>
+                    <div>{convertTimestamp(sliderLabels[0])} - {convertTimestamp(sliderLabels[1])}</div>
+                    <ReactSlider
+                    className="time-range-selector-slider"
+                    thumbClassName="slider-thumb"
+                    trackClassName="slider-track"
+                    defaultValue={[8, 17]}
+                    ariaLabelledby={['first-slider-label', 'second-slider-label']}
+                    ariaValuetext={state => `Thumb value ${state.valueNow}`}
+                    renderThumb={(props, _) => <div {...props}></div>}
+                    pearling
+                    minDistance={1}
+                    min={0}
+                    max={24}
+                    onAfterChange={sliderAfterChange}
+                    onChange={sliderOnChange}
+                    />
+                </div>
+                <p className="slider-instruction">
+                    Use the slider to adjust the the viewable area's start and end times.
+                </p>
+                </div>
 
                 <div className="form-group">
                 <label htmlFor="password">Password to Event Link (optional):</label>
